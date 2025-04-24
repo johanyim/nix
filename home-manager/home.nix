5e4198f8f4f970af08123b66b88f3b915bd878d7
johanyim <johanjyyim@gmail.com>
2025-04-24 21:32:21 +0100

    ADD: console colors

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,32 +1,28 @@
-{ config, lib, pkgs, inputs, ...}: {
-	imports = [
-		./modules/bundle.nix	
-	];
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [ ./modules/bundle.nix ];
 
-	home = {
-		username = "johan";
-		homeDirectory = "/home/johan";
-		stateVersion = "23.11";
-		packages = with pkgs; [
-			neofetch
-			htop
-			hyprpaper
-		];
-	};
+  home = {
+    username = "johan";
+    homeDirectory = "/home/johan";
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      neofetch
+      htop
+      hyprpaper
+    ];
+  };
 
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			rebuild = "sudo nixos-rebuild switch";
-		};
-	};
+  programs.git = {
+    enable = true;
+    userName = "johanyim";
+    userEmail = "johanjyyim@gmail.com";
+  };
 
-	programs.git = {
-		enable = true;
-		userName = "johanyim";
-		userEmail = "johanjyyim@gmail.com";
-	};
-
-
-	
 }
