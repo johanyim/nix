759798c47ece4ed3ac8d0ca9d136ffcf5dc04064
johanyim <johanjyyim@gmail.com>
2025-05-15 00:56:23 +0100

    Gen[143] at 2025-05-15 00:48:53

--- home-manager/scripts/hms.nix
+++ home-manager/scripts/hms.nix
@@ -3,8 +3,9 @@ pkgs.writeShellScriptBin "hms" ''
   ${pkgs.git}/bin/git add "$HOME/nix"
   ${pkgs.home-manager}/bin/home-manager switch --flake /home/johan/nix/flake.nix 
 
+
   if [ $? -eq 0 ]; then
-    MESSAGE=$(nix-env --list-generations | grep current | awk '{print "Generation: " $1 " at " $2 " " $3}')
+    MESSAGE=$(nix-env --list-generations | grep current | awk '{print "Gen-" $1 " at " $2 " " $3}')
     ${pkgs.git}/bin/git commit -m "$MESSAGE"
   fi
 
