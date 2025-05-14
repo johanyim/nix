00554f1a9e0c0af2e8144363aac9fa2f36415ce4
johanyim <johanjyyim@gmail.com>
2025-05-15 00:47:02 +0100

    Generation: 142 at 2025-05-15 00:46:56

--- home-manager/scripts/hms.nix
+++ home-manager/scripts/hms.nix
@@ -1,6 +1,6 @@
-{ pkgs, config }:
+{ pkgs }:
 pkgs.writeShellScriptBin "hms" ''
-  ${pkgs.git}/bin/git add ${config.users.users.username.home}/nix
+  ${pkgs.git}/bin/git add "$HOME/nix"
   ${pkgs.home-manager}/bin/home-manager switch --flake /home/johan/nix/flake.nix
 
   MESSAGE=$(nix-env --list-generations | grep current | awk '{print "Generation: " $1 " at " $2 " " $3}')
