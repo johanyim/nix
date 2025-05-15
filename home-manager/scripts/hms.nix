ddd3dbb72c5f40f08268ce2901d85bc870f75df2
johanyim <johanjyyim@gmail.com>
2025-05-15 01:19:50 +0100

    Gen-149 at 2025-05-15 01:19:50

--- home-manager/scripts/hms.nix
+++ home-manager/scripts/hms.nix
@@ -1,16 +1,16 @@
 { pkgs }:
 # ${pkgs.home-manager}/bin/home-manager switch --flake "$HOME/nix/flake.nix"
 pkgs.writeShellScriptBin "hms" ''
-  ${pkgs.git}/bin/git diff 
+  ${pkgs.git}/bin/git --no-pager diff 
   ${pkgs.git}/bin/git add "$HOME/nix"
-  ${pkgs.nh}/bin/nh home  switch "$HOME/nix"
 
 
-  if [ $? -eq 0 ]; then
-    MESSAGE=$(nix-env --list-generations \
+  MESSAGE=$(nix-env --list-generations \
   | grep current \
   | awk '{print "Gen[" $1 "] at " $2 " " $3}')
     ${pkgs.git}/bin/git commit --edit -m "$MESSAGE"
-  fi
 
+  if [ $? -eq 0 ]; then
+    ${pkgs.nh}/bin/nh home switch "$HOME/nix"
+  fi
 ''
