295274327b737565be77adc30fd425b988448454
johanyim <johanjyyim@gmail.com>
2025-05-29 00:33:20 +0100

    HomeManager[720] at 2025-05-29 00:33

--- home-manager/scripts/port_killer.nix
+++ home-manager/scripts/port_killer.nix
@@ -1,6 +1,11 @@
 { pkgs }:
 pkgs.writeShellScriptBin "port_killer" ''
-  ${pkgs.lsof}/bin/lsof -iTCP -sTCP:LISTEN -Pn | 
-  awk 'NR>1 {print $9, $1, $2}' |
-  ${pkgs.rofi}/bin/rofi -dmenu
+  selection=$(${pkgs.lsof}/bin/lsof -iTCP -sTCP:LISTEN -Pn | 
+  awk 'NR>1 {print $9"\t"$1"\t"$2}' | uniq | ${pkgs.rofi}/bin/rofi -dmenu -p "kill: ")
+
+  listener=$(echo "$selection" | awk -F'\t' '{ print $1 }')
+  pname=$(echo "$selection" | awk -F'\t' '{ print $2 }')
+  pid=$(echo "$selection" | awk -F'\t' '{ print $3 }')
+
+  kill "$pid" || kill -9 "$pid" && ${pkgs.libnotify}/bin/notify-send "$pname (PID: $pid) listening on $listener killed successfully"
 ''
