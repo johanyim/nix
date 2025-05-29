e91a4f4706e8e247b56874530538bc48ccb975f7
johanyim <johanjyyim@gmail.com>
2025-05-30 00:13:23 +0100

    HomeManager[752] at 2025-05-30 00:13

--- home-manager/modules/hypr/hyprland/scripts/open_notes.nix
+++ home-manager/modules/hypr/hyprland/scripts/open_notes.nix
@@ -20,7 +20,7 @@ pkgs.writeShellScriptBin "open_notes" ''
   	if [[ -f "$HOME/notes/$(date --i).md" ]]; then
   		${pkgs.alacritty}/bin/alacritty -e $EDITOR "$HOME/notes/$(date --i).md"
   	else
-  		${pkgs.alacritty}/bin/alacritty -e $EDITOR -c "put='# $(date --i)'" -c "put=$quotes" -c "put=$quotes" -c "normal! 3G" -c 'startinsert' "$HOME/notes/$(date --i).md"
+  		${pkgs.alacritty}/bin/alacritty -e $EDITOR -c "put='# $(date --i)'" -c "put=' '" -c "put=' '" -c "normal! 3G" -c 'startinsert' "$HOME/notes/$(date --i).md"
   	fi
   	exit 0
   	;;
