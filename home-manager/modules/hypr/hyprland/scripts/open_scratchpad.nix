236cff6fccdd8a3d1f41c9cf4b532334f130c333
johanyim <johanjyyim@gmail.com>
2025-05-29 23:50:57 +0100

    HomeManager[747] at 2025-05-29 23:50

--- home-manager/modules/hypr/hyprland/scripts/open_scratchpad.nix
+++ home-manager/modules/hypr/hyprland/scripts/open_scratchpad.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "open_scratchpad" ''
+  INSTANCE_NAME="scratchpad"
+  PID_PATH="/tmp/$INSTANCE_NAME.pid"
 
-  ${pkgs.alacritty}/bin/alacritty -T Scratchpad -e $EDITOR "$HOME/notes/scratchpad.md"
+  if [[ -f $PID_PATH ]]; then
+  	PID=$(cat $PID_PATH)
+
+  	rm -f "$PID"
+  	kill "$PID" >/dev/null 2>&1 && exit 0
+  fi
+
+  ${pkgs.alacritty}/bin/alacritty --title $INSTANCE_NAME --class "floating" --command "$EDITOR" "$HOME/notes/scratchpad.md" &
+  echo $! >"$PID_PATH"
 
 ''
