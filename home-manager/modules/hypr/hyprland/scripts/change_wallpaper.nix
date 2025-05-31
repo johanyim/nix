1748e2dc34ecc53b4cd37465613834ec136baf85
johanyim <johanjyyim@gmail.com>
2025-05-31 17:46:15 +0100

    HomeManager[778] at 2025-05-31 17:46

--- home-manager/modules/hypr/hyprland/scripts/change_wallpaper.nix
+++ home-manager/modules/hypr/hyprland/scripts/change_wallpaper.nix
@@ -4,6 +4,8 @@ let
 in
 pkgs.writeShellScriptBin "change_wallpaper" ''
   selection=$(ls -1 ${self + "/wallpaper/"} | rofi -dmenu)
+
+  notify-send "$selection"
   hyprctl hyprpaper preload $selection
   hyprctl hyprpaper wallpaper "${display},''${selection}"
 ''
