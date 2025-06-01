d02de307c4170356e78dd3f306bbdfc2134b9469
johanyim <johanjyyim@gmail.com>
2025-06-01 16:55:09 +0100

    HomeManager[782] at 2025-06-01 16:55

--- home-manager/modules/hypr/hyprland/scripts/change_wallpaper.nix
+++ home-manager/modules/hypr/hyprland/scripts/change_wallpaper.nix
@@ -3,7 +3,7 @@ let
   display = "eDP-1";
 in
 pkgs.writeShellScriptBin "change_wallpaper" ''
-  selection=$(ls -1 ${self + "/wallpaper/"} | rofi -dmenu)
+  selection=$(ls -1 ${self + "/wallpaper/"} | rofi -dmenu -p "Change Wallpaper")
   path="${self + "/wallpaper/"}$selection"
   notify-send "Wallpaper changed to $selection"
   hyprctl hyprpaper preload $path
