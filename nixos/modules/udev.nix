b9c1fdbf578edfa98021c3c96a2033d5e755ffe7
johanyim <johanjyyim@gmail.com>
2025-06-05 13:04:09 +0100

    NixOS[267] at 2025-06-04 10:52:19

--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
-{
-  services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_STATUS}=="Charging", RUN+="${pkgs.libnotify}/bin/notify-send hello"
+let
+  x = pkgs.writeShellScriptBin "thing" ''
+    ${pkgs.libnotify}/bin/notify-send "Battery is connected"
   '';
-
+in
+{
+  # UDEV  [226.648987] change   /devices/platform/USBC000:00/power_supply/ucsi-source-psy-USBC000:001 (power_supply)
+  services.udev.extraRules = ''
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${x}/bin/thing"
+  '';
 }
