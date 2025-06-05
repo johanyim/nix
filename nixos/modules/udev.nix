27aa75e7e032d8689bdf1bae692e55a2209160c8
johanyim <johanjyyim@gmail.com>
2025-06-05 23:52:08 +0100

    NixOS[267] at 2025-06-04 10:52:19

--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -11,7 +11,7 @@ in
 {
   # UDEV  [226.648987] change   /devices/platform/USBC000:00/power_supply/ucsi-source-psy-USBC000:001 (power_supply)
   services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ACTION=="change", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${pkgs.systemd}/bin/systemd-run --machine=johan@thinkpad --user notify-send 'Changing Power States' 'Using battery power'"
+    SUBSYSTEM=="power_supply", ACTION=="change", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${pkgs.systemd}/bin/systemd-run --machine=johan@.host --user notify-send 'Changing Power States' 'Using battery power'"
     SUBSYSTEM=="power_supply", ACTION=="change", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${thing}/bin/thing"
   '';
 }
