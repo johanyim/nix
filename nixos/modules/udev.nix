020825318bdf9b6ecb6b7353015982484d1e2988
johanyim <johanjyyim@gmail.com>
2025-06-05 23:31:26 +0100

    NixOS[267] at 2025-06-04 10:52:19

--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -23,7 +23,7 @@ in
 {
   # UDEV  [226.648987] change   /devices/platform/USBC000:00/power_supply/ucsi-source-psy-USBC000:001 (power_supply)
   services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${thing}/bin/thing"
-    SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${thing}/bin/thing"
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${thing}/bin/thing"
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${thing}/bin/thing"
   '';
 }
