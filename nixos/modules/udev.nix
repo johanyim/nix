15f79ef6bd9716102051193ad12a413fda55c7c1
johanyim <johanjyyim@gmail.com>
2025-06-05 23:25:58 +0100

    NixOS[267] at 2025-06-04 10:52:19

--- nixos/modules/udev.nix
+++ nixos/modules/udev.nix
@@ -1,14 +1,29 @@
 { pkgs, ... }:
 let
   thing = pkgs.writeShellScriptBin "thing" ''
-    ${pkgs.libnotify}/bin/notify-send "Battery is connected"
+    export DISPLAY=:0
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
+    ${pkgs.dbus}/bin/dbus-send \
+    --session \
+    --dest=org.freedesktop.Notifications \
+    --type=method_call \
+    /org/freedesktop/Notifications \
+    org.freedesktop.Notifications.Notify \
+    string:"udev" \
+    uint32:0 \
+    string:"dialog-information" \
+    string:"Charger status changed" \
+    string:"AC adapter plugged or unplugged." \
+    array:string: \
+    dict:string:string: \
+    int32:5000
   '';
 in
 {
   # UDEV  [226.648987] change   /devices/platform/USBC000:00/power_supply/ucsi-source-psy-USBC000:001 (power_supply)
   services.udev.extraRules = ''
-       SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${thing}/bin/thing"
+    SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="${thing}/bin/thing"
     SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="${thing}/bin/thing"
-
   '';
 }
