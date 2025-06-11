960a4079203ab722ae176fe32aea5b0b6cea0dc7
johanyim <johanjyyim@gmail.com>
2025-06-11 22:45:06 +0100

    HomeManager[811] at 2025-06-11 22:45

--- home-manager/modules/wezterm.nix
+++ home-manager/modules/wezterm.nix
@@ -45,6 +45,45 @@ in
       };
     };
 
+    extraConfig = # lua
+      ''
+        return {
+          audible_bell = "Disabled",
+
+          adjust_window_size_when_changing_font_size = false,
+          bold_brightens_ansi_colors = true,
+          cell_width = 0.9,
+          -- cell_width = 1,
+          line_height = 1.0,
+          -- line_height = 1.1,
+          cursor_blink_rate = 0,
+          disable_default_mouse_bindings = false,
+          -- enable_tab_bar = false,
+          hide_tab_bar_if_only_one_tab = true,
+          scrollback_lines = 5000,
+          window_close_confirmation = "NeverPrompt",
+
+          color_schemes = {
+            ["Catppuccin Mocha Ext"] = custom,
+          },
+          color_scheme = "catppuccin",
+          font = wezterm.font("NotoSansM Nerd Font"),
+          -- font = wezterm.font("ProFont IIx Nerd Font"),
+          -- font = wezterm.font("Tamzen"),
+          -- font = wezterm.font("Monocraft Nerd Font"),
+          -- font = wezterm.font("Miracode"),
+          harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+
+          keys = {
+            {
+              key = "Backspace",
+              mods = "CTRL",
+              action = wezterm.action.SendString("\x17"),
+            },
+          },
+        }
+      '';
+
   };
 
 }
