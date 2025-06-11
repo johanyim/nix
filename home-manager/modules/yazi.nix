7a2598fbf0912816d3d769f5cd2b299b31ad8b78
johanyim <johanjyyim@gmail.com>
2025-06-11 22:02:00 +0100

    HomeManager[809] at 2025-06-11 22:02

--- home-manager/modules/yazi.nix
+++ home-manager/modules/yazi.nix
@@ -9,6 +9,10 @@ let
   hex = lib.mapAttrs (name: hex: "#${hex}") baseColors;
 in
 {
-  programs.yazi.enable = true;
+  programs.yazi = {
+    enable = true;
+
+    enableZshIntegration = true;
+  };
 
 }
