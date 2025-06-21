5b717ad83164eeda95837c4593e57905805b6189
johanyim <johanjyyim@gmail.com>
2025-06-21 11:54:39 +0100

    NixOS[274] at 2025-06-14 14:45:57

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  hardware.bluetooth.enable = false; # setting to true causes a permission issue
+  hardware.bluetooth.enable = true; # setting to true causes a permission issue
   # workaround: adding user to the group 'lp'
   hardware.bluetooth.powerOnBoot = true;
 }
