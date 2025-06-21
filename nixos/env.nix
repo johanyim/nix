76ae46c1453457661dde95bb44546efc8f032442
johanyim <johanjyyim@gmail.com>
2025-06-21 13:08:25 +0100

    NixOS[276] at 2025-06-21 13:07:00

--- nixos/env.nix
+++ nixos/env.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   environment.variables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
     MANPAGER = "nvim -c 'Man!' -";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
 
 }
